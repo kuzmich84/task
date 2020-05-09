b37a239c45508e2f4b36308aced2085dcbd6ac67
Dmitry Kuchukov <kudim84@yandex.ru>
2020-05-10 02:50:22 +0300

    добавляет моки в filter

--- src/const.js
+++ src/const.js
@@ -1,1 +1,16 @@
 export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
+export const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+export const MONTHNAMES = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import {createMenu} from './components/menu.js';
-import {createFilter} from "./components/filter.js";
 import {createListTask} from "./components/list.js";
 import {createTask} from "./components/task.js";
 import {createEditTask} from "./components/task-edit.js";
 import {createButtonLoadMore} from "./components/load-more-button.js";
+import {createFilterTemplate} from "./components/filter.js";
+import {generateFilters} from "./mock/filter.js";
 
 const COUNTTASK = 3;
 
@@ -14,7 +15,7 @@ const render = (container, template, place) => {
 const menu = document.querySelector(`.main__control`);
 const main = document.querySelector(`.main`);
 render(menu, createMenu(), `beforeend`);
-render(main, createFilter(), `beforeend`);
+render(main, createFilterTemplate(generateFilters()), `beforeend`);
 render(main, createListTask(), `beforeend`);
 const boardTasks = document.querySelector(`.board__tasks`);
 render(boardTasks, createEditTask(), `beforeend`);
